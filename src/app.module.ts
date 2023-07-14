6e1fba90d32db42116ea3deff36ce420cad34a8c
Allef Carvalho <allefcarvalho@ymail.com>
2023-07-13 22:40:26 -0300

    authentication with jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
+import { UsersModule } from './users/users.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [    
+  imports: [
+    UsersModule,
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/portfolio'),
-    MoviesModule
+    MoviesModule,
+    AuthModule,
+
   ],
   controllers: [AppController],
   providers: [AppService],
